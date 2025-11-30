0597632c877f230e7133a24a71f93e1cbff95bc9
rosorillo <rosorillo@alu.edu.gva.es>
2025-11-30 11:46:25 +0100

    index.php modificado para que acceda directamente a login del juego

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ session_set_cookie_params([
 
 session_start();
 
+if (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    header("Location: login.php");
+    exit;
+}
+
 // si no se loguea como admin, no se puede entrar al panel de administración
 if (
     !(isset($_GET['controller']) && $_GET['controller'] === 'user' &&
